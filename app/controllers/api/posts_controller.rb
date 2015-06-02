53256fc84d4843872fd12036a8a4ea26b93cfd7a
Benjamin Linton <ben.linton@tablexi.com>
2015-06-02 09:02:37 -0500

    Add api posts controller methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,4 +1,54 @@
 module Api
   class PostsController < ApiController
+
+    def index
+      @posts = Post.all
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def create
+      @post = Post.new(post_params)
+
+      if @post.save
+        render_created
+      else
+        render_errors(@post)
+      end
+    end
+
+    def update
+      @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+        render_updated
+      else
+        render_errors(@post)
+      end
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+
+      if @post.destroy
+        render_destroyed
+      else
+        render_errors(@post)
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(
+        :title,
+        :excerpt,
+        :body,
+        :image_url,
+      )
+    end
+
   end
 end
