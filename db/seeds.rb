719298272aac87c647bf45019fe108fad75e00f1
Benjamin Linton <ben.linton@tablexi.com>
2015-06-02 09:22:21 -0500

    Create post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Post.any?
+
+  (1..4).each do |index|
+    Post.create(
+      title:      Faker::Lorem.sentence(2, false, 4),
+      excerpt:    Faker::Lorem.sentence(10, false, 20),
+      body:       Faker::Lorem.sentence(30, false, 100),
+      image_url:  "http://placehold.it/300&text=#{Faker::Lorem.word.titleize}"
+    )
+  end
+
+end
