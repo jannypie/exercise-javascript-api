41d3c9c837b03672c0e5e194b61a52f8963bd859
Benjamin Linton <ben.linton@tablexi.com>
2015-06-02 09:09:10 -0500

    Minor tweaks to api controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,24 +4,24 @@ module Api
     private
 
     def render_created
-      render(:show, status: :created)
+      render :show, status: :created
     end
 
 
     def render_updated
-      render(:show, status: :ok)
+      render :show, status: :ok
     end
 
 
     def render_destroyed
-      render(:show, status: :ok)
+      render :show, status: :ok
     end
 
 
     def render_errors(object)
-      render('api/shared/errors',
+      render "api/shared/errors",
         locals: {errors: object.errors},
-        status: :unprocessable_entity)
+        status: :unprocessable_entity
     end
 
   end
