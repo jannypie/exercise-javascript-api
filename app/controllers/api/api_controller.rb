e74046972c5564ace3c00ebac8f9ebcb8abaa09d
Benjamin Linton <ben.linton@tablexi.com>
2015-06-02 08:59:23 -0500

    Add methods for api_controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,28 @@
 module Api
   class ApiController < ApplicationController
+
+    private
+
+    def render_created
+      render(:show, status: :created)
+    end
+
+
+    def render_updated
+      render(:show, status: :ok)
+    end
+
+
+    def render_destroyed
+      render(:show, status: :ok)
+    end
+
+
+    def render_errors(object)
+      render('api/shared/errors',
+        locals: {errors: object.errors},
+        status: :unprocessable_entity)
+    end
+
   end
 end
